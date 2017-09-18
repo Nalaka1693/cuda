5986db148e32aa96010787163cacd3061c299923
Nalaka1693 <nalaka1693@gmai.com>
2017-09-18 23:45:42 +0530

    indented

--- arr_add2.cu
+++ arr_add2.cu
@@ -6,39 +6,40 @@
 #define SIZE 10
 
 __global__ void fillArr(int arr[]);
+
 __global__ void opArr(int arr1[], double arr2[]);
 
 int main() {
-        int i = 0;
-        time_t t;
-        srand((unsigned) time(&t));
+    int i = 0;
+    time_t t;
+    srand((unsigned) time(&t));
 
-        int arr1[SIZE];
-        double arr2[(int)SIZE/2];
+    int arr1[SIZE];
+    double arr2[(int) SIZE / 2];
 
-        for (i = 0; i < SIZE; ++i)      {
-                arr1[i] = rand() % SIZE;
-        }
+    for (i = 0; i < SIZE; ++i) {
+        arr1[i] = rand() % SIZE;
+    }
 
-        int *dArr1;
-        double *dArr2;
-        cudaMalloc((void **)&dArr1, sizeof(int) * SIZE);
-        cudaMalloc((void **)&dArr2, sizeof(double) * SIZE);
+    int *dArr1;
+    double *dArr2;
+    cudaMalloc((void **) &dArr1, sizeof(int) * SIZE);
+    cudaMalloc((void **) &dArr2, sizeof(double) * SIZE);
 
-        cudaMemcpy(dArr1, arr1, sizeof(int) * SIZE, cudaMemcpyHostToDevice);
-        opArr<<<i, SIZE/2>>>(dArr1, dArr2);
-        cudaMemcpy(arr2, dArr2, sizeof(double) * SIZE, cudaMemcpyDeviceToHost);
+    cudaMemcpy(dArr1, arr1, sizeof(int) * SIZE, cudaMemcpyHostToDevice);
+    opArr << < i, SIZE / 2 >> > (dArr1, dArr2);
+    cudaMemcpy(arr2, dArr2, sizeof(double) * SIZE, cudaMemcpyDeviceToHost);
 
-        for (i = 0; i < (int)SIZE/2; ++i)       {
-                printf("%4d     %.2f\n", i, arr2[i]);
-        }
-        printf("\n");
+    for (i = 0; i < (int) SIZE / 2; ++i) {
+        printf("%4d     %.2f\n", i, arr2[i]);
+    }
+    printf("\n");
 
-        return 0;
+    return 0;
 }
 
 __global__ void opArr(int arr1[], double arr2[]) {
-        int i = threadIdx.x;
+    int i = threadIdx.x;
 
-        arr2[i] = (arr1[2*i] + arr2[2*i+1]) / 2;
+    arr2[i] = (arr1[2 * i] + arr2[2 * i + 1]) / 2;
 }
--- vec_add.cu
+++ vec_add.cu
@@ -10,71 +10,70 @@ So this time we make use of multiple blocks to make the program work for vectors
 
 #define SIZE 2048 * 64
 
-__global__  void sumVector1(int *vectorAns_cuda, int *vectorA_cuda);
+__global__ void sumVector1(int *vectorAns_cuda, int *vectorA_cuda);
 
 int main() {
 
-        //arrays in main memory
-        int vectorA[SIZE];
-        int vectorB[SIZE];
-        int vectorAns[SIZE];
+    //arrays in main memory
+    int vectorA[SIZE];
+    int vectorB[SIZE];
+    int vectorAns[SIZE];
 
-        //generate some values
-        int i;
-        for(i=0;i<SIZE;i++) {
-           vectorA[i]=i;
-           vectorB[i]=SIZE-i;
-        }
+    //generate some values
+    int i;
+    for (i = 0; i < SIZE; i++) {
+        vectorA[i] = i;
+        vectorB[i] = SIZE - i;
+    }
 
-        //pointers for arrays to be put on cuda memory
-        int *vectorA_cuda;
-        int *vectorB_cuda;
-        int *vectorAns_cuda;
+    //pointers for arrays to be put on cuda memory
+    int *vectorA_cuda;
+    int *vectorB_cuda;
+    int *vectorAns_cuda;
 
-        //allocate memory in cuda device
-        cudaMalloc((void **)&vectorA_cuda,sizeof(int)*SIZE);
-        cudaMalloc((void **)&vectorAns_cuda,sizeof(int)*SIZE/2);
+    //allocate memory in cuda device
+    cudaMalloc((void **) &vectorA_cuda, sizeof(int) * SIZE);
+    cudaMalloc((void **) &vectorAns_cuda, sizeof(int) * SIZE / 2);
 
-        //copy contents from main memory to cuda device memory
-        cudaMemcpy(vectorA_cuda,vectorA,sizeof(int)*SIZE,cudaMemcpyHostToDevice);
+    //copy contents from main memory to cuda device memory
+    cudaMemcpy(vectorA_cuda, vectorA, sizeof(int) * SIZE, cudaMemcpyHostToDevice);
 
-        //thread configuration
-        /* Here we arbitrarily specify 256 threads per block
-           Then we divide the number of threads by 256 and round it off to the next int to get the number of blocks needed
-        */
-        int numBlocks = ceil(SIZE/(float)256);
-        int threadsPerBlock = 256;
+    //thread configuration
+    /* Here we arbitrarily specify 256 threads per block
+       Then we divide the number of threads by 256 and round it off to the next int to get the number of blocks needed
+    */
+    int numBlocks = ceil(SIZE / (float) 256);
+    int threadsPerBlock = 256;
 
-        //call the cuda kernel
-        sumVector1<<<numBlocks,threadsPerBlock>>>(vectorAns_cuda, vectorA_cuda, vectorB_cuda);
+    //call the cuda kernel
+    sumVector1 <<< numBlocks, threadsPerBlock >>> (vectorAns_cuda, vectorA_cuda, vectorB_cuda);
 
-        //copy back the results from cuda memory to main memory
-        cudaMemcpy(vectorAns,vectorAns_cuda,sizeof(int)*SIZE/2,cudaMemcpyDeviceToHost);
+    //copy back the results from cuda memory to main memory
+    cudaMemcpy(vectorAns, vectorAns_cuda, sizeof(int) * SIZE / 2, cudaMemcpyDeviceToHost);
 
-        //print the answer
-        printf("Answer is : ");
+    //print the answer
+    printf("Answer is : ");
 
-        for(i=0;i<SIZE;i++){
-           printf("%d ",vectorAns[i]);
-        }
-        printf("\n");
+    for (i = 0; i < SIZE; i++) {
+        printf("%d ", vectorAns[i]);
+    }
+    printf("\n");
 
-        return 0;
+    return 0;
 }
 
 /** CUDA kernel to add two vectors*/
-__global__  void sumVector1(int *vectorAns_cuda, int *vectorA_cuda){
+__global__ void sumVector1(int *vectorAns_cuda, int *vectorA_cuda) {
 
-        //threadIndex = blockSize * blockIndex + threadIndex
-        /* Here we have to calculate the position of the element in the array by using threadIndex, blockIndex and block Size*/
-        int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    //threadIndex = blockSize * blockIndex + threadIndex
+    /* Here we have to calculate the position of the element in the array by using threadIndex, blockIndex and block Size*/
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
 
-        //if the SIZE id not divisible by 256 there would be threads that are out of range the the array
-        // such threads should idle rather than doing any work
-        // So using a if condition we prevent them from doing any operation
-        if(tid < SIZE/2){
-           vectorAns_cuda[tid] = (vectorA_cuda[2*tid]+vectorB_cuda[2*tid+1])/2;
-        }
-
+    //if the SIZE id not divisible by 256 there would be threads that are out of range the the array
+    // such threads should idle rather than doing any work
+    // So using a if condition we prevent them from doing any operation
+    if (tid < SIZE / 2) {
+        vectorAns_cuda[tid] = (vectorA_cuda[2 * tid] + vectorB_cuda[2 * tid + 1]) / 2;
+    }
 }
 
